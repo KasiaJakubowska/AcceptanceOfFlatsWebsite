83693411734dd97777df114ee114904d4786c58c
KatarzynaJakubowska <katarzyna.jakubowska1998@gmail.com>
2022-07-20 00:16:54 +0200

    sectionContact

--- js/main.js
+++ js/main.js
@@ -28,4 +28,13 @@ navbar.forEach((element) => {
 		this.classList.add("active");
 	});
 });
+//FOOTER//
+const footerYear = document.querySelector(".footer__year");
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
 
