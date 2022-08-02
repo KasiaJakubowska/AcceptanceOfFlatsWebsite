7ff1404b59ea91406409698ce8ec92ce51a100e2
KatarzynaJakubowska <katarzyna.jakubowska1998@gmail.com>
2022-08-02 11:10:33 +0200

    form

--- js/main.js
+++ js/main.js
@@ -28,6 +28,67 @@ navbar.forEach((element) => {
 		this.classList.add("active");
 	});
 });
+
+//FORM
+const form = document.querySelector("form[name='contact-form']");
+const nameInput = document.querySelector("input[name='name']");
+const surnameInput = document.querySelector("input[name='surname']");
+const emailInput = document.querySelector("input[name='email']");
+const phoneInput = document.querySelector("input[name='phone']");
+const messageInput = document.querySelector("textarea[name='message']");
+
+nameInput.isValid = () => !!nameInput.value;
+surnameInput.isValid = () => !!surnameInput.value;
+emailInput.isValid = () => isValidEmail(emailInput.value);
+phoneInput.isValid = () => isValidPhone(phoneInput.value);
+messageInput.isValid = () => !!messageInput.value;
+
+const inputFields = [nameInput, surnameInput, emailInput, phoneInput, messageInput];
+
+const isValidEmail = (email) => {
+	const re =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+};
+
+const isValidPhone = (phone) => {
+	const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{3}$/im;
+	return re.test(String(phone).toLowerCase());
+};
+
+let shouldValidate = false;
+let isFormValid = false;
+
+const validateInputs = () => {
+	console.log("we are here");
+	if (!shouldValidate) return;
+
+	isFormValid = true;
+	inputFields.forEach((input) => {
+		input.classList.remove("invalid");
+		input.nextElementSibling.classList.add("hide");
+
+		if (!input.isValid()) {
+			input.classList.add("invalid");
+			isFormValid = false;
+			input.nextElementSibling.classList.remove("hide");
+		}
+	});
+};
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	shouldValidate = true;
+	validateInputs();
+	if (isFormValid) {
+		// TODO: DO AJAX REQUEST
+	}
+});
+
+inputFields.forEach((input) => input.addEventListener("input", validateInputs));
+
+
+
 //FOOTER//
 const footerYear = document.querySelector(".footer__year");
 
@@ -37,4 +98,3 @@ const handleCurrentYear = () => {
 };
 
 handleCurrentYear();
-
